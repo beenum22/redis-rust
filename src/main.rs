d188020952e5825c4894e54645571c3f678b62c1
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2024-11-22 21:25:43 +0100

    feat: Add basic support for Ping

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
 #![allow(unused_imports)]
+use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener;
+use std::net::IpAddr;
+use std::net::TcpStream;
+use std::str::FromStr;
 
-fn main() {    
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
+struct RedisServer {
+    addr: IpAddr,
+    port: u16
+}
+
+impl RedisServer {
+    fn new(addr: &str, port: u16) -> Self {
+        Self {
+            addr: IpAddr::from_str(addr).unwrap(),
+            port: port
+        }
+    }
+
+    fn run(&self) -> () {
+        let listener = TcpListener::bind(format!("{}:{}", self.addr.to_string(), self.port)).unwrap();
+        println!("Redis Server is running on {}:{}", self.addr.to_string(), self.port);
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    // TODO: Read PING specifically later
+                    stream.write_all(b"+PONG\r\n").unwrap()
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
+}
+
+
+fn main() {    
+    let redis_server = RedisServer::new("127.0.0.1", 6379);
+    redis_server.run()
 }
